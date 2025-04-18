d299792f98e3ea473be3bff141435d4c43b023ae
aditya dey <deyaditya34@gmail.com>
2025-04-18 09:53:24 +0530

    first commit

---
+++ src/auth/auth.login.api.js
@@ -0,0 +1,27 @@
+const authService = require("./auth.service");
+const buildApiHandler = require("../api-utils/build-api-handler");
+const paramsValidator = require("../middlewares/params-validator");
+
+async function controller(req, res) {
+  const { username, password } = req.body;
+  console.log("username -", username);
+  const token = await authService.login(username, password);
+
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+  });
+
+  res.json({
+    success: true,
+    data: "login successful",
+  });
+}
+
+const missingParamsValidator = paramsValidator.createParamValidator(
+  ["username", "password"],
+  paramsValidator.PARAM_KEY.BODY
+);
+
+module.exports = buildApiHandler([missingParamsValidator, controller]);
---
+++ src/auth/auth.register.api.js
@@ -0,0 +1,28 @@
+const authService = require("./auth.service");
+const buildApiHandler = require("../api-utils/build-api-handler");
+const paramsValidator = require("../middlewares/params-validator");
+const userResolver = require("../middlewares/user-resolver");
+const checkAdminRights = require("../middlewares/check-admin-rights");
+const {validateUsername} = require("./auth.utils");
+
+async function controller(req, res) {
+  const { username, password } = req.body;
+  
+  await authService.register(username, password);
+
+  res.json({
+    success: true,
+    data: "user registered successfully",
+  });
+}
+
+const missingParamsValidator = paramsValidator.createParamValidator(
+  ["username", "password"],
+  paramsValidator.PARAM_KEY.BODY
+);
+
+module.exports = buildApiHandler([
+  missingParamsValidator,
+  validateUsername,
+  controller
+]);
